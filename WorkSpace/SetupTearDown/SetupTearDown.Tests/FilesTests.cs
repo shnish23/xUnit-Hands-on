d71ea037658e865d345a1e73330796f3168ade18
shinichi.nishihata <nishihata@motionpicture.jp>
2020-08-26 19:42:15 +0900

    test:初期化処理と終了処理

--- WorkSpace/SetupTearDown/SetupTearDown.Tests/FilesTests.cs
+++ WorkSpace/SetupTearDown/SetupTearDown.Tests/FilesTests.cs
@@ -6,8 +6,35 @@ namespace SetupTearDown.Tests
 {
     public class FilesTests : IDisposable
     {
+        private const string ExistFileName = "test.txt";
+        private const string TextFileContent = "Hello, xUnit.net!";
+        private const string NotExistFileName = "NotExistFile";
+
+        public FilesTests()
+        {
+            if (File.Exists(ExistFileName))
+                File.Delete(ExistFileName);
+
+            File.WriteAllText(ExistFileName, TextFileContent);
+        }
+
         public void Dispose()
         {
+            if (File.Exists(ExistFileName))
+                File.Delete(ExistFileName);
+        }
+
+        [Fact]
+        public void DeleteIfExistWhenExistFile()
+        {
+            Assert.True(Files.DeleteIfExist(ExistFileName));
+            Assert.False(File.Exists(ExistFileName));
+        }
+
+        [Fact]
+        public void DeleteIfExistWhenNotExistFile()
+        {
+            Assert.False(Files.DeleteIfExist("NotExistFile"));
         }
     }
 }
--- WorkSpace/SetupTearDown/SetupTearDown/Files.cs
+++ WorkSpace/SetupTearDown/SetupTearDown/Files.cs
@@ -5,5 +5,15 @@ namespace SetupTearDown
 {
     public static class Files
     {
+        public static bool DeleteIfExist(string file)
+        {
+            if (!File.Exists(file))
+            {
+                return false;
+            }
+
+            File.Delete(file);
+            return true;
+        }
     }
 }
