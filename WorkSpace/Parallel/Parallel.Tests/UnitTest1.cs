b544eb25713c162f7494dcdd93d152503fea6e28
shinichi.nishihata <nishihata@motionpicture.jp>
2020-08-26 19:52:27 +0900

    test:並列テストの実行

--- WorkSpace/Parallel/Parallel.Tests/UnitTest1.cs
+++ WorkSpace/Parallel/Parallel.Tests/UnitTest1.cs
@@ -4,7 +4,23 @@ using Xunit;
 
 namespace Parallel.Tests
 {
+    [Collection("Our Test Collection #1")]
     public class UnitTest1
     {
+        [Fact]
+        public void Test1()
+        {
+            Thread.Sleep(3000);
+        }
+    }
+
+    [Collection("Our Test Collection #1")]
+    public class UnitTest2
+    {
+        [Fact]
+        public void Test2()
+        {
+            Thread.Sleep(5000);
+        }
     }
 }
