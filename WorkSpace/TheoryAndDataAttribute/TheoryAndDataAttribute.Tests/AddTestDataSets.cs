34b4dd9ccd4b6257573ef4e37e074ab70c19f72a
shinichi.nishihata <nishihata@motionpicture.jp>
2020-08-26 20:32:44 +0900

    test:TheoryとDataAttribute

--- WorkSpace/TheoryAndDataAttribute/TheoryAndDataAttribute.Tests/AddTestDataSets.cs
+++ WorkSpace/TheoryAndDataAttribute/TheoryAndDataAttribute.Tests/AddTestDataSets.cs
@@ -3,7 +3,20 @@ using System.Collections.Generic;
 
 namespace TheoryAndDataAttribute.Tests
 {
-    class AddTestDataSets
+    class AddTestDataSets : IEnumerable<object[]>
     {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            return new List<object[]>
+                {
+                    new object[]{1, 2, 3},
+                    new object[]{-1, -2, -3},
+                }.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 }
